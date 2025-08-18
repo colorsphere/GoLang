f64dc26d2f279cb3dab246c529f7b2d650e3e149
Mikhail Moroazov <morozovmv@yahoo.com>
2025-08-18 20:10:33 +0700

    Homework 18.08

---
+++ 4-11HW.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+	read()
+}
+
+func read() {
+	quantity := 0.0
+	fmt.Scan(quantity)
+}
+
+func calculation(quantity float64, usd string, eur string) {
+
+}
