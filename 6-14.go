a1d6b9eafd2e769ee271278a00e6ea054234db65
Mikhail Moroazov <morozovmv@yahoo.com>
2025-08-28 21:03:58 +0700

    1

--- 6-14.go
+++ 6-14.go
@@ -2,20 +2,26 @@ package main
 
 import (
 	"fmt"
+	"strconv"
 	"strings"
 )
 
 func main() {
-	calculate(readOperation())
-
+	calculate(readString(), readOperation())
 }
 
 func readString() (numbers []float64) {
 	var str string
 	fmt.Print("Введите последовательность чисел через запятую: ")
 	fmt.Scan(&str)
-	strings.Split(str, ",")
-
+	strs := strings.Split(str, ",")
+	for _, value := range strs {
+		number, err := strconv.Atoi(value)
+		if err == nil {
+			numbers = append(numbers, float64(number))
+		}
+	}
+	return
 }
 
 func readOperation() (operation string) {
@@ -29,7 +35,17 @@ func readOperation() (operation string) {
 	return
 }
 
-func calculate(operation string) (result float64) {
-	fmt.Print(operation)
+func calculate(numbers []float64, operation string) (result float64) {
+	fmt.Println(numbers)
+	fmt.Println(operation) // удалить
+	switch operation {
+	case "SUM", "sum":
+		fmt.Print("Сумма чисел равна: ")
+	case "AVG", "avg":
+		fmt.Print("Среднее значение равно: ")
+	case "MED", "med":
+		fmt.Print("Медиана равна: ")
+		numbers := 
+	}
 	return
 }
