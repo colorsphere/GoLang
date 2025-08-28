49a906b93c36e75845f441a6ae6ec096823ee941
Mikhail Moroazov <morozovmv@yahoo.com>
2025-08-28 20:38:55 +0700

    1

---
+++ 6-14.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	calculate(readOperation())
+
+}
+
+func readString() (numbers []float64) {
+	var str string
+	fmt.Print("Введите последовательность чисел через запятую: ")
+	fmt.Scan(&str)
+
+}
+
+func readOperation() (operation string) {
+	fmt.Print("Введите операцию (AVG, SUM, MED):")
+	for operation != "AVG" && operation != "avg" && operation != "SUM" && operation != "sum" && operation != "MED" && operation != "med" {
+		fmt.Scan(&operation)
+		if operation != "AVG" && operation != "avg" && operation != "SUM" && operation != "sum" && operation != "MED" && operation != "med" {
+			fmt.Print("Неверный ввод, повторите (AVG, SUM, MED):")
+		}
+	}
+	return
+}
+
+func calculate(operation string) (result float64) {
+	fmt.Print(operation)
+	return
+}
---
+++ str2int.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	str := "1,3,5,6,8,42"
+
+	strs := strings.Split(str, ",")
+	var ints []int
+	for _, s := range strs {
+		num, err := strconv.Atoi(s)
+		if err == nil {
+			ints = append(ints, num)
+		}
+	}
+
+	fmt.Println(ints)
+}
