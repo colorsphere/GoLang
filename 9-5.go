373a7c90fd58f3ad6e2ddd41db1f64925fcfcda9
Mikhail <morozovmv@yahoo.com>
2025-09-03 16:19:39 +0700

    123

---
+++ 9-5.go
@@ -0,0 +1,36 @@
+package main // struct пример объявления
+
+import "fmt"
+
+type account struct {
+	login    string // это список полей, название поля + тип поля
+	password string
+	url      string
+}
+
+func main() {
+	login := promptData("Введите логин: ")
+	password := promptData("Введите пароль: ")
+	url := promptData("Введите URL: ")
+
+	myAcount := account{
+		login:    login,    // данные из переменных заносятся в поля структуры
+		password: password, //
+		url:      url,
+	}
+	outputPassword(&myAcount)
+
+}
+
+func promptData(prompt string) string {
+	fmt.Print(prompt)
+	var res string
+	fmt.Scan(&res)
+	return res
+}
+
+func outputPassword(acc *account) {
+	fmt.Println(acc) // выводится указатель
+	//	acc.login = "!!"
+	fmt.Println(acc.login, acc.password, acc.url) // аналогичная запись выглядит fmt.Println((*acc).login, (*acc).password, (*acc).url) // выводятся значения
+}
