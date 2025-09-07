77cc8b052a370f11eb8175729fb193127ed73a30
Mikhail Morozov <morozovmv@yahoo.com>
2025-09-07 17:30:24 +0700

    123

---
+++ main.go
@@ -0,0 +1,25 @@
+package main // Композиция
+
+import (
+	"fmt"
+)
+
+func main() {
+	login := promptData("Введите логин: ")
+	password := promptData("Введите пароль: ")
+	url := promptData("Введите URL: ")
+	myAcount, err := newAccountWithTimeStamp(login, password, url)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	myAcount.outputPassword() // так вызывается метод, передавать ничего не нужно
+	fmt.Println(*myAcount)
+}
+
+func promptData(prompt string) string {
+	fmt.Print(prompt)
+	var res string
+	fmt.Scanln(&res)
+	return res
+}
