fe3a3a66470242558f77a9a13f11c3350bae77c1
Mikhail Morozov <morozovmv@yahoo.com>
2025-09-18 19:15:57 +0700

    123

--- 12/output/errors.go
+++ 12/output/errors.go
@@ -1,5 +1,4 @@
-package output // any type switch
-
+package output // вытаскиваем тип value
 import (
 	"fmt"
 
@@ -8,6 +7,18 @@ import (
 
 func PrintError(value any) {
 	//	fmt.Println(value)
+	intval, ok := value.(int)
+	if ok {
+		fmt.Println("int", intval, ok)
+	}
+	strval, ok := value.(string)
+	if ok {
+		fmt.Println("string", strval, ok)
+	}
+	errval, ok := value.(error)
+	if ok {
+		fmt.Println("error", errval, ok)
+	}
 	switch t := value.(type) {
 	case string:
 		fmt.Println("string", t)
